29c03f976640f4752c346b97f1433002241f79a9
Logan Howard <logan@oflogan.com>
2015-07-23 07:59:58 -0700

    make toggle compile

--- src/elisabet/toggle.clj
+++ src/elisabet/toggle.clj
@@ -1,16 +1,24 @@
 (ns elisabet.toggle
   (:gen-class
-   :use elisabet.constants))
+   :state state
+   :init init)
+  (:require [elisabet.constants :as const]))
 
 (import edu.wpi.first.wpilibj.Timer)
 
 
-(def timeouts (atom {}))
+(defn- init
+  []
+  [[] (atom {})])
+
+(defn- timeouts
+  [this]
+  (.state this))
+
 (defn- add-timeout
   "Adds a k-v button-timer pair to a map"
   [button timer]
-  (reset! timeouts (merge timeouts {button timer})))
-
+  (reset! (timeouts) (merge (timeouts) {button timer})))
 
 (defn add-timeout
   "Adds new timeout / refreshes existing timeout."
@@ -22,12 +30,12 @@
 (defn get-timeout
   "Get timer for button, or nil if nonexistent."
   [button]
-  (timeouts button))
+  ((timeouts) button))
 
 (defn has-time-left
   "Returns whether or not there is time left in the button toggle."
   ([button]
-   (has-time-left button TOGGLE_TIME))
+   (has-time-left button const/TOGGLE_TIME))
   ([button seconds]
    (let [timer (get-timeout button)]
      (and timer (< (.get timer) seconds)))))
