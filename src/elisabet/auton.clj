046cef0db8b7b65ec8a4f735623e3d30d748b6d9
Logan Howard <logan@oflogan.com>
2015-07-31 23:38:54 -0700

    add macro for fun and profit and increased debug time

--- src/elisabet/auton.clj
+++ src/elisabet/auton.clj
@@ -3,20 +3,25 @@
    :name auton)
   (:require (elisabet [drivetrain :as drivetrain])))
 
-(declare between drive)
+(declare between drive run-for)
 
 (defn drive-over-step
   "Drive over the step into the auton zone."
   [timer base]
-  (let [secs      (.get timer)
-        max-secs  2.9
-        between  #(between secs %1 %2)
-        time     #(* % max-secs)]
-    (cond
-      (between (time 0.0) (time 0.2)) (drive base (- 0.3))
-      (between (time 0.2) (time 0.8)) (drive base (- 0.5))
-      (between (time 0.8) (time 1.0)) (drive base (- 0.25))
-      :else                           (drivetrain/stop base))))
+  (run-for 2.9
+           (cond
+             (between (time 0.0) (time 0.2)) (drive base 0.3)
+             (between (time 0.2) (time 0.8)) (drive base 0.5)
+             (between (time 0.8) (time 1.0)) (drive base 0.25)
+             :else                           (drivetrain/stop base))))
+
+(defmacro run-for
+  [max-secs & exprs]
+  `(let [~'secs     (.get ~'timer)
+         ~'max-secs ~max-secs
+         ~'between #(between ~'secs %1 %2)
+         ~'time    #(* % ~'max-secs)]
+     ~@exprs))
 
 (defn- between
   "Returns value if value is between a and b, else nil"
@@ -28,4 +33,4 @@
 (defn- drive
   "Move forward with a given power"
   [base forward]
-  (drivetrain/drive-raw base forward 0))
+  (drivetrain/drive-raw base (- forward) 0))
