97cec6b5bcabc79ca73363b353c4dd7c8b076a09
Logan Howard <logan@oflogan.com>
2015-08-07 18:38:44 -0700

    add some files taken from decompiled from "normal" java frc program lol this is sketchy af

--- project.clj
+++ project.clj
@@ -3,11 +3,21 @@
   :url "https://github.com/glinia/3501-spark-clojure"
   :license {:name "GNU GPL v3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.namespace "0.2.11"]]
+
   :aot :all
+  :main edu.wpi.first.wpilibj.RobotBase
+  :manifest {"Robot-Class" "elisabet.robot"
+             "Class-Path"  "."}
+  :filespecs [{:type :paths :paths ["resources/wpilibj"]}]
+
+  :aliases {"deploy" ["./deploy"]}
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}}
+
   :resource-paths ["resources/*"])
